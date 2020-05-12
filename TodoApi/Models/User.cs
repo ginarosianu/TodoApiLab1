0082f20e52cb5f4da516b6b4720a9f30759595b7
gina <gina.rosianu@gmail.com>
2020-05-12 22:10:31 +0300

    Add project files.

---
+++ TodoApi/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserApi.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        [Required]
+        [MinLength(2)]
+        public string Name { get; set; }
+        [Range(18, 90)]
+        public int Age { get; set; }
+        [Required]
+        public string Address { get; set; }
+        public bool Active { get; set; }
+        
+
+    }
+}
---
+++ TodoApi/Models/UsersContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UserApi.Models
+{
+    public class UsersContext : DbContext
+    {
+        public UsersContext(DbContextOptions<UsersContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
